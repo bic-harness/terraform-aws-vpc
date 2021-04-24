2261aef3c04f3647c0c5297b8f85f1d1f2dae755
bic-harness <bogdan.catana@harness.io>
2021-04-24 06:56:15 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -217,9 +217,9 @@ resource "aws_route_table" "public" {
 resource "aws_route" "public_internet_gateway" {
   count = var.create_vpc && var.create_igw && length(var.public_subnets) > 0 ? 1 : 0
 
-  route_table_id         = aws_route_table.public[count.index].id
+  route_table_id         = aws_route_table.public[0].id
   destination_cidr_block = "0.0.0.0/0"
-  gateway_id             = aws_internet_gateway.this[count.index].id
+  gateway_id             = aws_internet_gateway.this[0].id
 
   timeouts {
     create = "5m"
@@ -229,9 +229,9 @@ resource "aws_route" "public_internet_gateway" {
 resource "aws_route" "public_internet_gateway_ipv6" {
   count = var.create_vpc && var.create_igw && var.enable_ipv6 && length(var.public_subnets) > 0 ? 1 : 0
 
-  route_table_id              = aws_route_table.public[count.index].id
+  route_table_id              = aws_route_table.public[0].id
   destination_ipv6_cidr_block = "::/0"
-  gateway_id                  = aws_internet_gateway.this[count.index].id
+  gateway_id                  = aws_internet_gateway.this[0].id
 }
 
 #################
